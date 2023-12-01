63dc6a6290de4555291cedc619f1b42fbe379e4f
Steeve Wille <swille@ipl.lu>
2023-12-01 17:38:08 +0100

    fixls re

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -57,7 +57,7 @@ echo "Root Key Password: $ROOT_KEY_PASS" && echo $ROOT_KEY_PASS > Results/rootCA
 echo "CSR Key Password: $CSR_KEY_PASS" && echo $CSR_KEY_PASS > Results/csr.key.pass
 
 # Clean up
-rm -rf Results/Keys Results/CAcerts Results/certs Results/CSR Results/Pass  crt.conf certs.tar.gz
+rm -rf Results/Keys Results/CAcerts Results/certs Results/CSR Results/Pass  crt.conf certs.tar.gz csr.key.pass rootCA.key.pass
 
 # Exit
 exit 0
