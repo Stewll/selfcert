a481ef93b8b1e52b04922f7d13eb4ef589c2b828
Steeve Wille <swille@ipl.lu>
2023-12-08 14:43:02 +0100

    Fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -30,8 +30,6 @@ openssl req -new -key Results/Keys/csr.key -out Results/CSR/csr.csr -passin pass
 # Sign CSR with Root CA
 openssl x509 -req -in Results/CSR/csr.csr -CA Results/CAcerts/root.pem -CAkey Results/Keys/rootCA.key \
     -CAcreateserial -out Results/certs/certificate.crt -days $validity -sha256 -passin pass:$ROOT_KEY_PASS
-# Create PFX file for Keepass containing the root key and the root certificate
-openssl pkcs12 -export -out Results/pfx_file_for_keepass/dynamics.pfx -inkey Results/Keys/rootCA.key -in Results/CAcerts/root.pem -passout pass:$ROOT_KEY_PASS
 
 
 echo ""
@@ -58,12 +56,14 @@ tar -czf certs.tar.gz Results/ crt.conf  && openssl enc -pbkdf2 -aes-256-cbc -md
 echo "To decrypt the tarball, run the following command:"
 echo "openssl enc -pbkdf2 -aes-256-cbc -md sha512 -d -salt -in Results/certs.tar.gz.enc -out certs.tar.gz -pass pass:$ROOT_KEY_PASS" 
 echo ""
-echo "Root Key Password: $ROOT_KEY_PASS" && echo $ROOT_KEY_PASS > Results/rootCA.key.pass
-echo "CSR Key Password: $CSR_KEY_PASS" && echo $CSR_KEY_PASS > Results/csr.key.pass
+echo "Root Key Password: $ROOT_KEY_PASS" && echo $ROOT_KEY_PASS > Results/Pass/rootCA.key.pass
+echo "CSR Key Password: $CSR_KEY_PASS" && echo $CSR_KEY_PASS > Results/Pass/csr.key.pass
+echo " " 
+echo "Creating a PFX file for Keepass containing the root key and the root certificate:"
+openssl pkcs12 -export -out Results/pfx_file_for_keepass/dynamics.pfx -inkey Results/Keys/rootCA.key -in Results/CAcerts/root.pem -passin pass:$ROOT_KEY_PASS -passout pass:$ROOT_KEY_PASS
 
 # Clean up
-rm -rf Results/Keys Results/CAcerts Results/certs Results/CSR Results/Pass  crt.conf certs.tar.gz csr.key.pass rootCA.key.pass
-
+rm -rf Results/Keys Results/CAcerts Results/certs Results/CSR Results/Pass  crt.conf certs.tar.gz Results/certs.tar.gz.enc Results/pfx_file_for_keepass 
 # Exit
 exit 0
 
